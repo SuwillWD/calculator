54771a4678ab82eec6ba7fccb8bc37c5d4627b4a
SuwillWD <136084114+SuwillWD@users.noreply.github.com>
2023-08-09 23:24:51 +0530

    Implement basic operations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const buttons = document.querySelectorAll(".row button");
 const display = document.querySelector(".display");
-const opr = document.querySelectorAll(".row .operator");
-const equals = document.querySelector(".row .operator equality");
+const opr = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equality");
+const numbers = document.querySelectorAll(".row .number");
 
 function add(a, b) {
   return a + b;
@@ -16,39 +16,76 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a * b;
+  return a / b;
 }
 
 function remainder(a, b) {
   return a % b;
 }
 
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
+let operator = null;
+let resetScreen = false;
 
 function operate(num1, num2, op) {
   switch (op) {
     case "+":
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case "-":
-      subtract(num1, num2);
-      break;
+      return subtract(num1, num2);
     case "*":
-      multiply(num1, num2);
-      break;
+      return multiply(num1, num2);
     case "/":
-      divide(num1, num2);
-      break;
+      if (num2 === "0") {
+        return null;
+      }
+      return divide(num1, num2);
+    case "%":
+      return remainder(num1, num2);
+    default:
+      return "ERROR";
   }
 }
 
-function seprateExp(string) {}
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    display.textContent += button.textContent;
-    let exp = display.textContent;
-  });
+numbers.forEach((number) => {
+  number.addEventListener("click", () => displayNumber(number.textContent));
 });
+
+opr.forEach((op) => {
+  op.addEventListener("click", () => setOperator(op.textContent));
+});
+
+equals.addEventListener("click", evaluate);
+
+function displayNumber(number) {
+  if (display.textContent === "0" || resetScreen) {
+    blackDisplay();
+  }
+  display.textContent += number;
+}
+
+function blackDisplay() {
+  display.textContent = "";
+  resetScreen = false;
+}
+
+function setOperator(op) {
+  if (operator !== null) {
+    evaluate;
+    return;
+  }
+  firstNumber = Number(display.textContent);
+  operator = op;
+  resetScreen = true;
+}
+
+function evaluate() {
+  if (operator === null || resetScreen) return;
+  if (operator === "/" && display.textContent === "0") {
+    alert("ERROR: Division by 0");
+  }
+  secondNumber = Number(display.textContent);
+  display.textContent = operate(firstNumber, secondNumber, operator);
+  operator = null;
+}
